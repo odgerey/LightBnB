646bb75197f7e8941971e54005d6aded1ff66bec
odgerey <audreymedaino@gmail.com>
2020-06-25 02:09:49 +0000

    Added queries

---
+++ 1_queries/property_listings_by_city.sql
@@ -0,0 +1,17 @@
+SELECT properties.*, avg(property_reviews.rating) as average_rating
+FROM properties
+JOIN property_reviews ON properties.id = property_id
+WHERE city LIKE '%ancouv%'
+GROUP BY properties.id
+HAVING avg(property_reviews.rating) >= 4
+ORDER BY cost_per_night
+LIMIT 10;
+
+-- SELECT properties.*, avg(property_reviews.rating) as average_rating
+-- FROM properties
+-- JOIN property_reviews ON properties.id = property_id
+-- WHERE city LIKE '%iedmon%'
+-- GROUP BY properties.id
+-- HAVING avg(property_reviews.rating) >= 4
+-- ORDER BY cost_per_night
+-- LIMIT 10;
---
+++ 1_queries/user_login.sql
@@ -0,0 +1,11 @@
+SELECT id, name, email, password
+FROM users
+WHERE email = 'tristanjacobs@gmail.com';
+
+
+-- SELECT id, name, email, password
+-- FROM users
+-- WHERE email = 'reels@kg.com';
+
+
+
